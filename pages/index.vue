4cafcffd3b0d05725e965b5c52f0581c16da9f5c
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-05-24 11:29:18 +0100

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline"> Cria seu perfil </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Nome do usuario"
+              :counter="10"
+              :rules="nameRules"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Qual a sua agencia?"
+              :counter="10"
+              :rules="nameRules"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Numero de conta"
+              :counter="10"
+              :rules="nameRules"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Saldo inicial"
+              :counter="10"
+              :rules="nameRules"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions> </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
